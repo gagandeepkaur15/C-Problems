47cb5dee175237f22794700ba25d08d5a3616110
Gagandeep <gagandeepkaur1523@gmail.com>
2023-03-28 19:35:41 +0530

    Merge two sorted arrays leetcode problem solution

---
+++ merge_two_sorted_array_leetcode.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=0, j=0, temp=0;
+        while(i<m && j<n && nums1[i]!=0){
+            if(nums1[i]<nums2[j] ){
+                i++;
+            }
+            else if(nums1[i]>=nums2[j]){
+                temp=nums1[i+1];
+                nums1[i+1]=nums1[i];
+                nums1[i]=nums2[j];
+                nums1[i+2]=temp;
+                i+=2,j++;
+            }
+        }
+        while(j<n){
+            nums1[i]=nums2[j];
+            i++,j++;
+        }
+
+        for (int i:nums1){
+            cout<<i<<" ";
+        }
+}
+
+int main(){
+    vector<int> arr1;
+    arr1.push_back(1);
+    arr1.push_back(2);
+    arr1.push_back(3);
+    arr1.push_back(0);
+    arr1.push_back(0);
+    arr1.push_back(0);
+
+    vector<int> arr2;
+    arr2.push_back(2);
+    arr2.push_back(5);
+    arr2.push_back(6);
+
+    merge(arr1, 6, arr2, 3);
+}
